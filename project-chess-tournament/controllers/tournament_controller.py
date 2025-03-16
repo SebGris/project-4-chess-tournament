609afe43001d2ff6d8c934596389fa831ecb81af
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-03-16 19:21:23 +0100

    Refactorisation du TournamentController pour simplifier l'accès à la vue. Remplacement des références à tournament_view par view et ajout d'une méthode pour afficher les détails des tournois. Mise à jour des appels de méthode pour refléter ces changements.

--- project-chess-tournament/controllers/tournament_controller.py
+++ project-chess-tournament/controllers/tournament_controller.py
@@ -18,7 +18,7 @@ class TournamentController:
     ):
         self.tournament_repository = tournament_repository
         self.player_repository = player_repository
-        self.tournament_view = view
+        self.view = view
         self.round_view = RoundView()
         self.tournaments = self.get_tournaments()
         self.active_tournament = None
@@ -34,18 +34,18 @@ class TournamentController:
 
     def collect_tournament_info(self):
         return {
-            "name": self.tournament_view.get_name(),
-            "location": self.tournament_view.get_location(),
-            "start_date": self.tournament_view.get_start_date(),
-            "end_date": self.tournament_view.get_end_date(),
-            "number_of_rounds": self.tournament_view.get_number_of_rounds(),
+            "name": self.view.get_name(),
+            "location": self.view.get_location(),
+            "start_date": self.view.get_start_date(),
+            "end_date": self.view.get_end_date(),
+            "number_of_rounds": self.view.get_number_of_rounds(),
         }
 
     def create_new_tournament(self):
-        """Create a new tournament.""" 
+        """Create a new tournament."""
         tournament_info = self.collect_tournament_info()
         self.create_tournament(**tournament_info)
-        self.tournament_view.display_tournament_created(tournament_info["name"])
+        self.view.display_tournament_created(tournament_info["name"])
 
     def create_tournament(self, name, location, start_date, end_date, number_of_rounds):
         tournament = Tournament(name, location, start_date, end_date, number_of_rounds)
@@ -55,7 +55,7 @@ class TournamentController:
         self.tournament_repository.save_tournaments(self.tournaments)
 
     def select_tournament(self):
-        index = self.tournament_view.get_tournament_selection(self.tournaments)
+        index = self.view.get_tournament_selection(self.tournaments)
         self.active_tournament = self.tournaments[index]
 
     def load_all_players(self):
@@ -66,11 +66,11 @@ class TournamentController:
             }
             return all_players
         except FileNotFoundError as e:
-            self.tournament_view.display_for_file_not_found(str(e))
+            self.view.display_for_file_not_found(str(e))
 
     def add_players(self):
         players_data = []
-        while player_data := self.tournament_view.get_player_data():
+        while player_data := self.view.get_player_data():
             players_data.append(player_data)
         players = [Player(**data) for data in players_data]
         self.active_tournament.add_players(players)
@@ -92,10 +92,10 @@ class TournamentController:
 
     def __check_if_start(self):
         if not self.active_tournament.players:
-            self.tournament_view.display_start_error_without_players()
+            self.view.display_start_error_without_players()
             return False
         elif self.__check_if_odd(len(self.active_tournament.players)):
-            self.tournament_view.display_start_error_even_players()
+            self.view.display_start_error_even_players()
             return False
         return True
 
@@ -122,26 +122,26 @@ class TournamentController:
         for player1, player2 in pairs:
             new_round.add_match(player1, player2)
         self.active_tournament.rounds.append(new_round)
-        self.tournament_view.display_added_round_message(round_name)
+        self.view.display_added_round_message(round_name)
 
     def update_number_of_rounds(self):
-        new_number = self.tournament_view.get_number_of_rounds()
+        new_number = self.view.get_number_of_rounds()
         self.active_tournament.set_number_of_rounds(new_number)
-        self.tournament_view.display_updated_number_rounds_message(new_number)
+        self.view.display_updated_number_rounds_message(new_number)
 
     def update_description(self):
-        description = self.tournament_view.get_tournament_description()
+        description = self.view.get_tournament_description()
         self.active_tournament.set_description(description)
-        self.tournament_view.display_successful_description_message()
+        self.view.display_successful_description_message()
 
     def enter_scores(self):
         round_instance = self.active_tournament.get_current_round()
-        self.tournament_view.display_record_results_message(round_instance.name)
+        self.view.display_record_results_message(round_instance.name)
         for match in round_instance.matches:
             if round_instance.is_finished():
                 continue
-            self.tournament_view.display_match_summary(match.get_player_names())
-            result = self.tournament_view.get_match_result()
+            self.view.display_match_summary(match.get_player_names())
+            result = self.view.get_match_result()
             if result == "1":
                 match.set_score(1, 0)
             elif result == "2":
@@ -151,25 +151,29 @@ class TournamentController:
         round_instance.end_round()
 
     def display_available_tournaments(self):
-        self.tournament_view.display_tournaments(self.tournaments)
+        self.view.display_tournaments(self.tournaments)
 
     def display_active_tournament(self):
-        self.tournament_view.display_tournament_details(self.active_tournament)
+        self.view.display_tournament_details(self.active_tournament)
 
     def display_players(self):
-        self.tournament_view.display_players(self.active_tournament.players)
+        self.view.display_players(self.active_tournament.players)
 
     def display_player_names(self):
-        self.tournament_view.display_players_name(self.active_tournament.players)
+        self.view.display_players_name(self.active_tournament.players)
 
     def display_current_round(self):
         current_round = self.active_tournament.get_current_round()
         if current_round:
             self.round_view.display_round_info(current_round)
-            self.tournament_view.display_current_round(self.active_tournament)
+            self.view.display_current_round(self.active_tournament)
 
     def display_player_pairs(self):
         current_round = self.active_tournament.get_current_round()
         if current_round:
             round_name, pairs = current_round.get_pairs_players()
             self.round_view.display_player_pairs(round_name, pairs)
+
+    def display_tournament_details(self):
+        for tournament in self.tournaments:
+            self.view.display_tournament_details(tournament)
--- project-chess-tournament/menu_commands/show_tournaments_details_command.py
+++ project-chess-tournament/menu_commands/show_tournaments_details_command.py
@@ -7,18 +7,4 @@ class ShowTournamentsDetailsCommand(Command):
         self.controller = controller
 
     def execute(self):
-        if self.controller.tournaments:
-            for tournament in self.controller.tournaments:
-                tournament_dic = {
-                    "name": tournament.name,
-                    "location": tournament.location,
-                    "start_date": tournament.start_date,
-                    "end_date": tournament.end_date,
-                    "description": tournament.description,
-                    "number_of_rounds": tournament.number_of_rounds,
-                }
-                self.controller.tournament_view.display_tournament_details(
-                    tournament_dic
-                )
-        else:
-            self.controller.tournament_view.display_no_tournament_message()
+        self.controller.display_tournament_details()
