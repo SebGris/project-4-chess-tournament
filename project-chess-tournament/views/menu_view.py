55a07b41742c0faecfb0dbadbbf1c66b433f072e
Sebastien Grison <sebastien.grison@ik.me>
2025-03-22 15:13:07 +0100

    Ajout d'une ligne vide à l'initialisation de MenuView et refactorisation de l'affichage des séparateurs dans le menu pour améliorer la lisibilité.

--- project-chess-tournament/views/menu_view.py
+++ project-chess-tournament/views/menu_view.py
@@ -2,6 +2,11 @@ from views.base_view import BaseView
 
 
 class MenuView(BaseView):
+    def __init__(self):
+        super().__init__()
+        # affiche une ligne vide
+        print()
+
     def get_user_choice(self):
         return self.input("Choisissez une option :")
 
@@ -10,13 +15,15 @@ class MenuView(BaseView):
 
     def display_menu(self, menu):
         """Display a menu with a title and items."""
+        def create_separator_line(header):
+            print("=" * len(header))
         index = 1
         for group in menu.get_groups():
             menu_group_header = f"=== {group['title']} ==="
             if group['items'] == []:
-                print("=" * len(menu_group_header))
+                create_separator_line(menu_group_header)
             print(menu_group_header)
             for item in group['items']:
                 print(f"{index}. {item['label']}")
                 index += 1
-            print("=" * len(menu_group_header))
+            create_separator_line(menu_group_header)
